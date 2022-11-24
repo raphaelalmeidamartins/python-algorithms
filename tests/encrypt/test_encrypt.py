e42541c3e2fc7eb68f5d2d448604628de203f638
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-24 20:42:17 -0300

    :bug: fix: change assertions order

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,14 +3,14 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
+    assert encrypt_message('AABBCC', -1) == "CCBBAA"
+
+    assert encrypt_message('AABBCC', 3) == "BAA_CCB"
+
+    assert encrypt_message('ABBCCA', 4) == "AC_CBBA"
+
     with pytest.raises(TypeError, match="tipo inválido para message"):
-        encrypt_message(2, 1)
+        encrypt_message(123, 2)
 
     with pytest.raises(TypeError, match="tipo inválido para key"):
-        encrypt_message("anyt message", "invalid type key")
-
-    assert encrypt_message("AABBCC", -1) == "CCBBAA"
-
-    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
-
-    assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
+        encrypt_message("any message", "invalid type key")
