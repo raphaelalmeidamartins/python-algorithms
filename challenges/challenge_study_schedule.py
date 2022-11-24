1461f1ad0c0bed85a9c981ae7cf94520e048a6be
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-24 20:10:36 -0300

    :sparkles: feat: study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) is not int:
+        return None
+
+    students_counter = 0
+
+    for entrance_time, exit_time in permanence_period:
+        if type(entrance_time) is not int or type(exit_time) is not int:
+            return None
+
+        elif entrance_time <= target_time <= exit_time:
+            students_counter += 1
+
+    return students_counter
