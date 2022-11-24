150ab55260d635f800a4466f58a2470e948bddd5
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-24 20:23:40 -0300

    :test_tube: test: encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(2, 1)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("anyt message", "invalid type key")
+
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
+
+    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
+
+    assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
