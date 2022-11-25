c1d80ad0276f5ed6f33fcea82117bd546cffb974
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-24 22:22:58 -0300

    :sparkles: feat: find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
+from collections import Counter
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(nums) is not list or all(
+        [
+            type(num) is not int or num < 1
+            for num in nums
+        ]
+    ):
+        return False
+
+    num, count = Counter(nums).most_common()[0]
+
+    if count > 1:
+        return num
+
+    return False
